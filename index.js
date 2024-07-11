bbbd1ce66c4ae16881bbf57c205acca02fbee066
Gld-Mx24483 <israeltoki7@gmail.com>
2024-07-11 12:17:35 +0100

    "Removed unused code and updated CORS origin in index.js, and removed ENV variable and reformatted .env file"

--- index.js
+++ index.js
@@ -1,48 +1,5 @@
-// // index.js
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const cors = require('cors');
-// const path = require('path');
-// const galleryAPI = require('./gallery-api');
-// const teamAPI = require('./team-api');
-// const volunteerAPI = require('./volunteer-api');
-// const contactAPI = require('./contact-api');
-// const contentManagementAPI = require('./content-management-api');
-// const eventManagementAPI = require('./event-management-api');
-// const app = express();
-// // const { MongoClient } = require('mongodb');
-
-// app.use(cors());
-// app.use(bodyParser.json({ limit: '100000mb' }));
-// app.use(bodyParser.urlencoded({  limit: '100000mb', extended: true }));
-// app.use(express.json());
-
-
-// app.use('/', teamAPI);
-// app.use('/', galleryAPI);
-// app.use('/', contactAPI);
-// app.use('/', contentManagementAPI);
-// app.use('/', volunteerAPI);
-// app.use('/', eventManagementAPI);
-// app.use("/", (req,res) => {
-//   res.send("Server deployed and running on vercel.");
-// })
-
-// // app.use(cors({
-// //   origin: 'https://dmfc.vercel.app'
-// // }));
-
-// app.use(cors({
-//   origin: 'http://localhost:9000'
-// }));
-
-// const port = process.env.PORT || 9000;
-// app.listen(port, () => {
-//   console.log(`Server is running on port ${port}`);
-// });
-
 // index.js
-require('dotenv').config(); // Load environment variables from .env file
+require('dotenv').config();
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -73,8 +30,12 @@ app.use("/", (req,res) => {
   res.send("Server deployed and running on vercel.");
 });
 
+// app.use(cors({
+//   origin: 'http://localhost:9000'
+// }));
+
 app.use(cors({
-  origin: 'http://localhost:9000'
+  origin: 'https://dmfc-server.vercel.app'
 }));
 
 const port = process.env.PORT || 9000;
